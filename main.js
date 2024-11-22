2ee8f0b75e4b176f5d4e331697a2b44b9270d0d9
tortulin <mahirashid2012@gmail.com>
2024-11-21 21:33:53 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+let id = 0;
+let display = $('#display');
+let next = $('#next');
+let slide = ['tok','pisne', 'calabash'];
 
+display.textContent(slide[id]);
+next.on('click', function(){
+  id++;
+  display.textContent(slide[id]);
+});
